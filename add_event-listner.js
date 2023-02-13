97e90308ca0ab3f6e52a70a76b59f836490aba06
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-02-13 17:31:07 +0600

    add text and increase number

--- add_event-listner.js
+++ add_event-listner.js
@@ -15,4 +15,24 @@ const id = document.getElementById("id");
 document.querySelector(".button_id").addEventListener("click", () => {
   id.innerText = "I am also work";
 });
-// Number 2 is the end and number 3 is the beginning of the stop watch project
+
+// Adding Content part
+// Add hello text
+document.getElementById("add_hello_btn").addEventListener("click", () => {
+  const add_hello_text = document.getElementById("add_hello_text");
+  const add_hello_text_innerText = add_hello_text.innerText;
+  add_hello_text.textContent = `${add_hello_text_innerText} Hello`;
+  console.log(add_hello_text_innerText);
+});
+
+// increase number
+document
+  .getElementById("number_increase_button")
+  .addEventListener("click", () => {
+    const increase_number = document.getElementById("increase_number");
+    const getInnerText = increase_number.innerText;
+    const convertToNumber = parseInt(getInnerText);
+    increase_number.innerText = convertToNumber + 1;
+    console.log(convertToNumber);
+  });
+// Number 4 is the end and number 5 is the beginning of the stop watch project
