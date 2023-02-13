dd6f86be739ca7850f18c8480b532913ac0c9a7a
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-02-13 06:14:26 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// document.querySelector(".class").textContent =
+//   "I'm the querySelector for class and this is wark";
+
+// document.getElementById("id_query").textContent =
+//   "I'm the querySelector for the Id and it's work";
+
+// document.getElementById("id").innerText = "The getElementById and it's work";
+
+// Another way
+//-----------//
+
+const class_const = document.querySelector(".class");
+class_const.textContent = "I'm the querySelector for class and this is wark";
+
+const id_query_const = document.querySelector("#id_query");
+id_query_const.textContent = "I'm the querySelector for the Id and it's work";
+
+const getElementById_const = document.getElementById("id");
+getElementById_const.textContent = "The getElementById and it's work";
+// Number 1 is the end and number 2 is the beginning of the stop watch project
