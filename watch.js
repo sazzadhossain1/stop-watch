891cd8bcad04eadfa34722f587ddc37b9db59ca9
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-02-14 17:01:02 +0600

    time start and time stop done

--- watch.js
+++ watch.js
@@ -1,7 +1,7 @@
 let ms = 0;
 let s = 0;
 let m = 0;
-
+let counter;
 const start = document.querySelector(".start");
 const stop = document.querySelector(".stop");
 const reset = document.querySelector(".reset");
@@ -9,9 +9,25 @@ const reset = document.querySelector(".reset");
 const time = document.querySelector(".time");
 
 start.addEventListener("click", function () {
+  if (!counter) {
+    counter = setInterval(run, 10);
+  }
   function run() {
     time.textContent = m + ":" + s + ":" + ms;
     ms++;
+    if (ms === 100) {
+      ms = 0;
+      s++;
+    }
+    if (s === 60) {
+      s = 0;
+      m++;
+    }
   }
+});
+
+stop.addEventListener("click", function () {
+  clearInterval(counter);
+  counter = false;
 });
 // Number 5 is the end and number 6 is the beginning of the stop watch project
